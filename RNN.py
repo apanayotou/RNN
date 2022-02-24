8ba79c76ff80c9f07b351b51e452be8e02fe0aa6
Alex Panayotou <alexander.panayotou@gmail.com>
2022-02-24 17:33:43 +0200

    added doc string to some functions

--- RNN.py
+++ RNN.py
@@ -29,7 +29,10 @@ from sklearn.metrics import r2_score
 def pull_data(year,start,end,min_visits=10, groupby="cat"):
     '''
     Imports the Alphamega database classes used for quering the database.
-    Then creates the sql query using the start and end date 
+    Then creates the sql query using the start and end date.
+    Finally it pulls the data from the database
+    
+    Note: This will only work if using a Alphamega VPN.
 
     Parameters
     ----------
@@ -45,9 +48,11 @@ def pull_data(year,start,end,min_visits=10, groupby="cat"):
     Returns
     -------
     df : Pandas DataFrame
-        DESCRIPTION.
+        Dataframe that has every customer cat pair within the date range greater than the min_visits
 
     '''
+    
+    # Adds path where alpha
     sys.path.insert(1, 'C:/Users/alexp/Desktop/Work/Other/Useful/alphamega_db_classes')
     import alphamega_db_classes as adb
     
@@ -88,6 +93,22 @@ def pull_data(year,start,end,min_visits=10, groupby="cat"):
     return df
 
 def create_gap_df(df):
+    '''
+    Calculates the gap from
+
+    Parameters
+    ----------
+    df : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    gap_df : TYPE
+        DESCRIPTION.
+    new_df : TYPE
+        DESCRIPTION.
+
+    '''
     df = df.sort_values(["Cus_CardNo","cat","Pos_TimeDate"],ascending=[True,True,False])
     df = df.reset_index(drop=True)
     df_copy = df.copy()
@@ -155,7 +176,22 @@ def train_test_split(gap_df,df,frac=0.05):
     train_ic = gap_df[["cat","Cus_CardNo"]].drop(test_ic.index, errors="ignore")
     test = df[(df.cat.isin(test_ic.cat)) & (df.Cus_CardNo.isin(test_ic.Cus_CardNo))]
     train = df[(df.cat.isin(train_ic.cat)) & (df.Cus_CardNo.isin(train_ic.Cus_CardNo))]
-    return test, train
+    
+    test = test.dropna()
+    train = train.dropna()
+    
+    test_x = test.drop(["cat", "Cus_CardNo",  "gap", "index_copy","qty_x","y"],axis=1)
+    train_x = train.drop(["cat", "Cus_CardNo",  "gap", "index_copy","qty_x","y"],axis=1)
+    
+    test_y = test["y"].to_numpy()
+    train_y = train["y"].to_numpy()
+
+    test_x_array = np.array(test_x.to_numpy().tolist()).astype('float32')
+    train_x_array = np.array(train_x.to_numpy().tolist()).astype('float32')
+    
+    
+    
+    return test_x_array, test_y, train_x_array, train_y
 
 def encoder_decoder(n_steps_in,n_features,n_steps_out):
     model = Sequential()
@@ -174,10 +210,27 @@ def simple_RNN(n_steps,n_features):
     model.add(Dropout(0.2))
     model.add(Bidirectional(LSTM(50, activation='relu', input_shape=(n_steps, n_features))))
     model.add(Dense(1, activation='linear'))
-    #model.compile(optimizer='adam', loss='mse', metric="mae")
     opt = tf.keras.optimizers.Adam(lr=0.00001)
     model.compile(loss='mean_squared_error', optimizer= opt, metrics=['mae'])
     return model
+
+def RNN(layers,n_steps,n_features,dropout=0.2,lr=0.00001):
+    model = Sequential()
+    for i in range(len(layers)):
+        nodes = layers[i]
+        if i+1 == len(layers):
+            model.add(Bidirectional(LSTM(nodes, activation='relu', input_shape=(n_steps, n_features))))
+        else:
+            model.add(Bidirectional(LSTM(nodes, activation='relu', input_shape=(n_steps, n_features),return_sequences=True)))
+        if dropout:
+            model.add(Dropout(dropout))
+    
+    model.add(Dense(1, activation='linear'))
+    opt = tf.keras.optimizers.Adam(lr=lr)
+    model.compile(loss='mean_squared_error', optimizer= opt, metrics=['mae'])
+    
+    return model
+
 
 def outliers(df,col):
     # Select the first quantile
@@ -198,21 +251,64 @@ def filter_outlier_custs(gap_df, df):
     df = df[~df.Cus_CardNo.isin(outlier_custs)]
     return df
 
+def plot_loss_graph(history):
+    axes = plt.axes()
+    axes.plot(history.history['loss'])
+    axes.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    
+def test_model_with_future():
+    df_future = pull_data(year=2021, start='2021-06-01', end ='2021-12-30')
+    gap_df_future, df_future = create_gap_df(df_future)
+    #df_merge_future = df_future.merge(gap_df_future, how="left",left_on=["Itm_Code","Cus_CardNo"],right_on=["Itm_Code","Cus_CardNo"])
+    gap_df_future = gap_df_future.reset_index()
+    df_future = preprocess_df2(df_future)
+
+    test_x_future = df_future.drop(["Itm_Code", "Cus_CardNo",  "gap", "gap_scale", "index_copy"],axis=1)
+    test_y_future = df_future["gap"].to_numpy()
+
+    test_x_future = np.reshape(test_x_future.to_numpy(), (test_x_future.shape[0], test_x_future.shape[1], 1))
+
+    #score = model.evaluate(test_x_future, test_y_future, verbose=0)
+    
+    y_pred = model.predict(test_x_future)
+
+    #r2 = r2_score(test_y_future, y_pred.flatten())
+    axes = plt.axes()
+
+    axes.scatter(test_y_future,y_pred)
+
+def limit_data(train_x, train_y, test_x, test_y, lim):
+    train_x = train_x[:lim]
+    train_y = train_y[:lim]
+    test_x = test_x[:lim]
+    test_y = test_y[:lim]
+    return train_x, train_y, test_x, test_y
+
+def filter_cat_by_amount_cust(df, min_cust=20):
+    df_grouped = df.groupby("cat").nunique()
+    cats_to_keep = df_grouped[df_grouped.Cus_CardNo>20].index
+    df = df[df.cat.isin(cats_to_keep)]
+    return df
 
 # fixed paramaters
 predict_next = 1
 using_past = 25
-epochs = 500
+epochs = 1
 batch_size = 64
 name = f"predict-next-{predict_next}-using-{using_past}-at-{int(time.time())}"
 filter_outlier = False
 load_from_db = False
+########
+
 
 # loads data from alphamega database
 if load_from_db:
     df = pull_data(year=2021, start='2021-01-01', end ='2021-03-30')
-    # grouping by category
-    df = df.groupby(["Pos_TimeDate","cat","Cus_CardNo"]).sum().reset_index()
     # removing blank category
     df = df[df.cat!='$  ']
     df.to_csv("cat_data.csv",index=False)
@@ -221,79 +317,23 @@ if load_from_db:
 else:
     df = pd.read_csv("cat_data.csv",parse_dates=["Pos_TimeDate"])
 
-df_grouped = df.groupby("cat").nunique()
+df = filter_cat_by_amount_cust(df,20)
 
-cats_to_keep = df_grouped[df_grouped.Cus_CardNo>20].index
-
-df = df[df.cat.isin(cats_to_keep)]
 gap_df, df = create_gap_df(df)
 
-#df = filter_outlier_custs(gap_df,df)
-
+df = filter_outlier_custs(gap_df,df)
 
 df_merge = df.merge(gap_df, how="left",left_on=["cat","Cus_CardNo"],right_on=["cat","Cus_CardNo"])
-gap_df = gap_df.reset_index()
 
+gap_df = gap_df.reset_index()
 
 df = preprocess_df2(df,qty=True)
 
+test_x_array, test_y, train_x_array, train_y = train_test_split(gap_df,df)
 
+#model = simple_RNN(using_past,2)
 
-test , train = train_test_split(gap_df,df)
-
-test = test.dropna()
-train = train.dropna()
-test_x = test.drop(["cat", "Cus_CardNo",  "gap", "index_copy","qty_x","y"],axis=1)
-train_x = train.drop(["cat", "Cus_CardNo",  "gap", "index_copy","qty_x","y"],axis=1)
-test_y = test["y"].to_numpy()
-train_y = train["y"].to_numpy()
-
-#lim = 10000
-#train_x = train_x[:lim]
-#train_y = train_y[:lim]
-#test_x = test_x[:lim]
-#test_y = test_y[:lim]
-test_x_array = np.array(test_x.to_numpy().tolist()).astype('float32')
-train_x_array = np.array(train_x.to_numpy().tolist()).astype('float32')
-
-model = simple_RNN(test_x.shape[1],2)
-
-
-
-filepath = "RNN_Final-{epoch:02d}-{mae:.3f}"  # unique file name that will include the epoch and the validation acc for that epoch
-checkpoint = ModelCheckpoint("models/{}.model".format(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')) # saves only the best ones
-
-
-
-
-
-# Save model
-
-'''
-df_future = pull_data(year=2021, start='2021-06-01', end ='2021-12-30')
-
-
-gap_df_future, df_future = create_gap_df(df_future)
-df_merge_future = df_future.merge(gap_df_future, how="left",left_on=["Itm_Code","Cus_CardNo"],right_on=["Itm_Code","Cus_CardNo"])
-gap_df_future = gap_df_future.reset_index()
-
-df_future = preprocess_df2(df_future)
-
-test_x_future = df_future.drop(["Itm_Code", "Cus_CardNo",  "gap", "gap_scale", "index_copy"],axis=1)
-test_y_future = df_future["gap"].to_numpy()
-
-test_x_future = np.reshape(test_x_future.to_numpy(), (test_x_future.shape[0], test_x_future.shape[1], 1))
-
-score = model.evaluate(test_x_future, test_y_future, verbose=0)
-
-
-y_pred = model.predict(test_x_future)
-
-r2 = r2_score(test_y_future, y_pred.flatten())
-axes = plt.axes()
-
-axes.scatter(test_y_future,y_pred)
-'''
+model = RNN([50,50,50],using_past,2,dropout=0.2,lr=0.0001)
 
 history = model.fit(train_x_array, 
                     train_y, 
@@ -302,24 +342,10 @@ history = model.fit(train_x_array,
                     verbose=1,
                     validation_data=(test_x_array, test_y)
                     )
-axes = plt.axes()
-#axes.set_ylim([1,3])
-axes.plot(history.history['loss'])
-axes.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+plot_loss_graph(history)
 
 y_pred = model.predict(test_x_array)
 
-#axes = plt.axes()
-#axes.set_ylim([0,10])
-#axes.set_xlim([0,10])
 plt.scatter(test_y,y_pred)
-
-
-
 
 
