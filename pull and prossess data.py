0967d5d0d3eeb7ea4b1d24ab6fef9ec46f7b179a
Alex Panayotou <alexander.panayotou@gmail.com>
2022-04-27 17:47:58 +0300

    Added month and weekday cosine sine to preprocessing and only keeping max date for each pair

--- pull and prossess data.py
+++ pull and prossess data.py
@@ -5,17 +5,16 @@ Created on Thu Jan 20 11:03:53 2022
 @author: alexp
 """
 
-
 import sys
 import pandas as pd
 import numpy as np
 
 from sklearn import preprocessing
+from matplotlib import pyplot as plt
 import time
 
 
-
-def pull_data(year,start,end,min_visits=10, groupby="cat"):
+def pull_data(year, start, end, min_visits=10):
     '''
     Imports the Alphamega database classes used for quering the database.
     Then creates the sql query using the start and end date.
@@ -45,11 +44,11 @@ def pull_data(year,start,end,min_visits=10, groupby="cat"):
     sys.path.insert(1, 'C:/Users/alexp/Desktop/Work/Other/Useful/alphamega_db_classes')
     import alphamega_db_classes as adb
 
-    if year%2 == 0:
+    if year % 2 == 0:
         year1 = year
-        year2 = year+1
+        year2 = year + 1
     else:
-        year1 = year-1
+        year1 = year - 1
         year2 = year
     year_str = f'{year1}_{year2}'
     db = adb.insight_sql_querier("Ins_AMe")
@@ -66,22 +65,20 @@ def pull_data(year,start,end,min_visits=10, groupby="cat"):
             concat(ICa_3DCode, Cus_CardNo) IN ( 
                 SELECT concat(ICa_3DCode, Cus_CardNo)
                 FROM [Ins_AMe].[dbo].[Ins_PosTransactions{year_str}]
-				LEFT JOIN [Ins_Item] ON [Ins_Item].Itm_Code = [Ins_PosTransactions{year_str}].Itm_Code
+			 	LEFT JOIN [Ins_Item] ON [Ins_Item].Itm_Code = [Ins_PosTransactions{year_str}].Itm_Code
                 WHERE Pos_TimeDate >= '{start}' and  Pos_TimeDate <= '{end}'
                 GROUP BY ICa_3DCode, Cus_CardNo
-                HAVING Cus_CardNo <> '' AND count(distinct Pos_TimeDate) >= 10 
+                HAVING Cus_CardNo <> '' AND count(distinct Pos_TimeDate) >= {min_visits} 
                 )
         GROUP BY ICa_3DCode, Cus_CardNo, Pos_TimeDate
         HAVING Cus_CardNo <> '' and  ICa_3DCode<> '969'             
              
         """)
 
-
     db.select_db_data(query)
     db.close_conn()
     df = db.get_data().copy()
     return df
-
 
 
 def filter_cat_by_amount_cust(df, min_cust=10):
@@ -103,12 +100,13 @@ def filter_cat_by_amount_cust(df, min_cust=10):
 
     '''
     # groups the dataframe by cat and gets the unique count of other columns
-    df_grouped = df.groupby(["Cus_CardNo","cat"]).nunique()
+    df_grouped = df.groupby(["Cus_CardNo", "cat"]).nunique()
     df_grouped = df_grouped.reset_index()
     # filters the group df by the min_cust and save the index of cat ids
-    cats_to_keep = df_grouped[df_grouped.Pos_TimeDate>min_cust]
+    cats_to_keep = df_grouped[df_grouped.Pos_TimeDate > min_cust]
     # filter the main df using the cats_to_keep
-    df = cats_to_keep[["Cus_CardNo","cat"]].merge(df,how="left",left_on=["Cus_CardNo","cat"],right_on=["Cus_CardNo","cat"])
+    df = cats_to_keep[["Cus_CardNo", "cat"]].merge(df, how="left", left_on=["Cus_CardNo", "cat"],
+                                                   right_on=["Cus_CardNo", "cat"])
     return df
 
 
@@ -133,13 +131,15 @@ def create_gap_df(df):
 
     '''
     # Sort values to have all transactions of the same customer and category are together and sorted in decending date order.
-    df = df.sort_values(["Cus_CardNo","cat","Pos_TimeDate"],ascending=[True,True,False])
+    df = df.sort_values(["Cus_CardNo", "cat", "Pos_TimeDate"], ascending=[True, True, False])
     # reset index 
     df = df.reset_index(drop=True)
     # duplicated dataframe
     df_copy = df.copy()
     # subtract 1 from duplicated dataframe
-    df_copy.index = df_copy.index-1
+    df_copy.index = df_copy.index - 1
+    df_copy.rename(columns={"Pos_TimeDate": "prev_date", "qty": "prev_qty"}, inplace=True)
+
     # make index a column in both dfs so we can use it to merge
     df_copy["index_copy"] = df_copy.index
     df["index_copy"] = df.index
@@ -147,17 +147,18 @@ def create_gap_df(df):
     # merging both df and its copy on cat cust and index to make new_df. 
     # new_df has two date colomns now and because index is shifted and df is sorted, 
     # date from old df will have be current date and date from copy will have previous date.
-    new_df = df.merge(df_copy,how="inner",right_on=("cat", "Cus_CardNo","index_copy"),left_on=("cat", "Cus_CardNo","index_copy"))
+    new_df = df.merge(df_copy, how="inner", right_on=("cat", "Cus_CardNo", "index_copy"),
+                      left_on=("cat", "Cus_CardNo", "index_copy"))
 
     # Subtrace current with previous date to get gap
-    new_df["gap"] = new_df.Pos_TimeDate_x - new_df.Pos_TimeDate_y
+    new_df["gap"] = new_df.Pos_TimeDate - new_df.prev_date
     # format to show gap in days.
     new_df.gap = new_df.gap.dt.days
 
     return new_df
 
 
-def add_past_time_period(df, t, cols):
+def add_past_time_period(df, t, cols, value_list=True):
     """
     Adds a column to df. This column contains a list of factors from time period [current_date - t]
     This is done using the same method as add_gap: copy df, shift index by -t, add cols data from that
@@ -183,15 +184,18 @@ def add_past_time_period(df, t, cols):
     copy.index_copy = copy.index_copy - t
     columns = cols + ["cat", "Cus_CardNo", "index_copy"]
     copy = copy[columns]  # keep only target cols
-    copy[t] = list(copy[cols].values)
+    if value_list:
+        copy[t] = list(copy[cols].values)
+    else:
+        for col in cols:
+            copy[f"{col}_{t}"] = copy[col]
     copy = copy.drop(columns=cols)
     df = df.merge(copy, how='left', right_on=("cat", "Cus_CardNo", "index_copy"),
                   left_on=("cat", "Cus_CardNo", "index_copy"))
     return df
 
 
-
-def preprocess_df(df, qty=False, scale="standard", scale_y=True):
+def preprocess_df(df, past_cols=["gap","qty"], scale_cols=["gap","qty"] scale="standard", scale_y=True, value_list=True, scaler=None):
     """
     Function that contains all preprocessing steps for the dataframe.
 
@@ -225,23 +229,18 @@ def preprocess_df(df, qty=False, scale="standard", scale_y=True):
         Dataframe with preprocessed data.
     """
     # list of columns we want to keep
-    cols = ["cat", "Cus_CardNo", "gap"]
-    # columns to add to sequence
-    past_cols = ["gap"]
-    # if qty is True, add qty to sequence and to columns to keep
-    if qty:
-        cols = cols + ["qty_x"]
-        past_cols = past_cols + ["qty_x"]
+    cols = ["cat", "Cus_CardNo","Pos_TimeDate"]+past_cols
     # keep only columns we want to use
     df = df[cols]
     # creates separate column for target we want to predict (next gap).
     df["y"] = df.gap
     df.dropna(inplace=True)
     # scale input features
-    if scale == "MinMax":
-        scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
-    elif scale == "standard":
-        scaler = preprocessing.StandardScaler()
+    if not scaler:
+        if scale == "MinMax":
+            scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
+        elif scale == "standard":
+            scaler = preprocessing.StandardScaler()
     # Scale input features if scale is not None
     if scale != "None":
         df.loc[:, past_cols] = scaler.fit_transform(df.loc[:, past_cols])
@@ -251,24 +250,39 @@ def preprocess_df(df, qty=False, scale="standard", scale_y=True):
     df.dropna(inplace=True)
     # Loop through each time period and create a column for each time period containing a list of features from period.
     for t in reversed(range(1, using_past + 1)):
-        df = add_past_time_period(df, t, past_cols)
+        df = add_past_time_period(df, t, past_cols, value_list=value_list)
     df.dropna(inplace=True)
+    # group by customer and category and find max date for each customer and category
+    grouped = df.groupby(["cat", "Cus_CardNo"])["Pos_TimeDate"].max()
+    # reset index so we can merge with df
+    grouped = grouped.reset_index()
+    # merge with df, so we can keep only customer item pairs that have a max date.
+    df = df.merge(grouped, how='inner', on=["cat", "Cus_CardNo", "Pos_TimeDate"])
     # shuffle dataframe
     df = df.sample(frac=1)
-    return df
+    return df, scaler
+
 
 def x_y_split(df):
     df = df.dropna()
-    x = df.drop(["cat", "Cus_CardNo",  "gap", "index_copy","y"],axis=1)
+    x = df.drop(["cat", "Cus_CardNo", "gap", "index_copy", "y", "Pos_TimeDate"], axis=1)
 
     if "qty_x" in x.columns:
-        x = x.drop(["qty_x"],axis=1)
-       
+        x = x.drop(["qty_x"], axis=1)
+
     y = df["y"].to_numpy()
     x = np.array(x.to_numpy().tolist()).astype('float32')
     return x, y
 
 
+def df_date_to_month_weekday(df):
+    month = df.Pos_TimeDate.dt.month
+    weekday = df.Pos_TimeDate.dt.weekday
+    df["month_sine"] = np.sin(month / 12 * 2 * np.pi)
+    df["weekday_sine"] = np.sin(weekday / 7 * 2 * np.pi)
+    df["month_cos"] = np.cos(month / 12 * 2 * np.pi)
+    df["weekday_cos"] = np.cos(weekday / 7 * 2 * np.pi)
+    return df
 
 
 # fixed paramaters
@@ -280,28 +294,28 @@ batch_size = 64
 filter_outlier = False
 load_from_db = True
 ########
+year = 2017
+month1 = "01"
+month2 = "06"
 
+print(f"Pulling {month1} to {month2} of {year} data")
+df = pull_data(year=year, start=f'{year}-{month1}-01', end=f'{year}-{month2}-30')
+# removing blank category
+df = df[df.cat != '$  ']
+print("adding gap")
+df = create_gap_df(df)
+df = df_date_to_month_weekday(df)
+print("preprocessing")
+df, scaler = preprocess_df(df, scale="standard", scale_y=False, value_list=True, past_cols=["gap","qty","month_sine","weekday_sine","month_cos","weekday_cos"])
 
-# loads data from alphamega databas
-for year in range(2017,2018):
-    for month1, month2 in [("01","06"),("07","12")]:
-        print(f"Pulling {month1} to {month2} of {year} data")
-        df = pull_data(year=year, start=f'{year}-{month1}-01', end =f'{year}-{month2}-30')
-        # removing blank category
-        df = df[df.cat!='$  ']
-        df = df.sample(frac=.8)
-        print("adding gap")
-        df = create_gap_df(df)
-        print("preprossesing")
-        df = preprocess_df(df,cat=False,qty=True,scale="standard",scale_y=False)
-        print("splitting x and y")
-        x, y = x_y_split(df)
-        print("saving x and y")
-        np.save(f"data/X_{year}_{month1}_to_{month2}.npy",x)
-        np.save(f"data/y_{year}_{month1}_to_{month2}.npy",y)
-        x=[]
-        y=[]
-        df=[]
-
-
-
+print("splitting")
+'''
+print("splitting x and y")
+x, y = x_y_split(df)
+print("saving x and y")
+np.save(f"data/X_{year}_{month1}_to_{month2}.npy", x)
+np.save(f"data/y_{year}_{month1}_to_{month2}.npy", y)
+x = []
+y = []
+df = []
+'''
